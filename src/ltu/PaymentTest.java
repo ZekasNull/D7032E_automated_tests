040b126ac08e53de411ce671e027191781b39d99
Linnea Larsson <philar-9@student.ltu.se>
2026-09-04 21:37:30 +0200

    test: implement one age-related test case

--- src/ltu/PaymentTest.java
+++ src/ltu/PaymentTest.java
@@ -4,12 +4,38 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class PaymentTest
-{
+import java.io.IOException;
+
+public class PaymentTest {
+    private PaymentImpl getPaymentImplInstanceCurrentDate() throws IOException
+    {
+        //TODO the provided CalendarImpl is always the current date
+        return new PaymentImpl(new CalendarImpl());
+    }
+
     @Test
     public void testSilly()
     {
         assertEquals(1, 1);
+    }
+
+    @Test
+    public void validSubsidy_justTurned20Boundary() throws IOException
+    {
+        PaymentImpl payimpl = this.getPaymentImplInstanceCurrentDate();
+        int grantedSubsidy =
+                TestNumConstants.StudentSupportType.FULL_TIME_LOAN.getAmountPerMonth() +
+                TestNumConstants.StudentSupportType.FULL_TIME_SUBSIDIARY.getAmountPerMonth();
+
+        Student bornTomorrow = new StudentBuilder().birthDate(2006, 9, 5).build();
+        Student birthdayStudent = new StudentBuilder().birthDate(2006, 9, 4).build();
+        Student overTheAge = new StudentBuilder().birthDate(2006, 9, 3).build();
+
+        //incredibly enough, it seems the "correct" implementation we got does not consider month and day
+        //see getAge in PaymentImpl.java
+        assertEquals(0, payimpl.getMonthlyAmount(bornTomorrow.ssn, bornTomorrow.income, bornTomorrow.studyRate, bornTomorrow.completionRatio));
+        assertEquals(grantedSubsidy, payimpl.getMonthlyAmount(birthdayStudent.ssn, birthdayStudent.income, birthdayStudent.studyRate, birthdayStudent.completionRatio));
+        assertEquals(grantedSubsidy, payimpl.getMonthlyAmount(overTheAge.ssn, overTheAge.income, overTheAge.studyRate, overTheAge.completionRatio));
     }
 
 }
