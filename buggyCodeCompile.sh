1f3ab7b415cbcddf064056239690a9561af0cd30
Linnea Larsson <philar-9@student.ltu.se>
2026-09-04 18:39:25 +0200

    initial commit part 2

---
+++ buggyCodeCompile.sh
@@ -0,0 +1,16 @@
+# Determine path separator based on OS
+if [ "$(uname)" = "Linux" ] || [ "$(uname)" = "Darwin" ]; then
+    SEP=":"
+else
+    SEP=";"
+fi
+
+echo "Compiling code..."
+javac -d ./bin -cp ./src ./src/ltu/Main.java ./src/ltu/CalendarImpl.java
+
+echo "Replacing PaymentImpl.class with buggy version"
+cp ./buggyCode/PaymentImpl.class ./bin/ltu/
+
+
+echo "Compiling tests..."
+javac -d ./bin -cp ./lib/org.junit4-4.3.1.jar${SEP}./src ./src/ltu/PaymentTest.java
---
+++ src/ltu/PaymentTest.java
@@ -0,0 +1,15 @@
+package ltu;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PaymentTest
+{
+    @Test
+    public void testSilly()
+    {
+        assertEquals(1, 1);
+    }
+
+}
