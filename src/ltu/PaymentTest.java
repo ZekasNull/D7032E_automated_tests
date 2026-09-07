f3932414dd0eeba79442b4d65ef8c716431349e2
Barbora Karlíková <barbora.karlik@gmail.com>
2026-09-07 12:16:08 +0200

    Tests n.2 - study requiremnts

--- src/ltu/PaymentTest.java
+++ src/ltu/PaymentTest.java
@@ -7,15 +7,13 @@ import org.junit.Test;
 import java.io.IOException;
 
 public class PaymentTest {
-    private PaymentImpl getPaymentImplInstanceCurrentDate() throws IOException
-    {
+    private PaymentImpl getPaymentImplInstanceCurrentDate() throws IOException {
         //TODO the provided CalendarImpl is always the current date
         return new PaymentImpl(new CalendarImpl());
     }
 
     @Test
-    public void testSilly()
-    {
+    public void testSilly() {
         assertEquals(1, 1);
     }
 
@@ -26,15 +24,15 @@ public class PaymentTest {
 
     /**
      * Evaluates 101
+     *
      * @throws IOException
      */
     @Test
-    public void validSubsidy_justTurned20Boundary() throws IOException
-    {
+    public void validSubsidy_justTurned20Boundary() throws IOException {
         PaymentImpl payimpl = this.getPaymentImplInstanceCurrentDate(); //FIXME use 2016 + adjust birthdays
         int grantedSubsidy =
                 TestNumConstants.StudentSupportType.FULL_TIME_LOAN.getAmountPerMonth() +
-                TestNumConstants.StudentSupportType.FULL_TIME_SUBSIDIARY.getAmountPerMonth();
+                        TestNumConstants.StudentSupportType.FULL_TIME_SUBSIDIARY.getAmountPerMonth();
 
         //exact birthday is not relevant
         Student tooYoung = new StudentBuilder().birthDate(2007, 1, 1).build();
@@ -52,13 +50,12 @@ public class PaymentTest {
      * Until 56 = <57
      */
     @Test
-    public void noSubsidyAfter56() throws IOException
-    {
+    public void noSubsidyAfter56() throws IOException {
         PaymentImpl payimpl = this.getPaymentImplInstanceCurrentDate(); //FIXME use 2016 + adjust birthdays
 
         Student ok_under = StudentBuilder.fullTimeStudentNoIncome()
-                                   .birthDate(1971, 1, 1) //55 years old
-                                   .build();
+                .birthDate(1971, 1, 1) //55 years old
+                .build();
         Student ok_border = StudentBuilder.fullTimeStudentNoIncome()
                 .birthDate(1970, 1, 1) //56 years old
                 .build();
@@ -70,11 +67,11 @@ public class PaymentTest {
 
 
         assertEquals(onlysubsidy,
-                     payimpl.getMonthlyAmount(ok_under.ssn, ok_under.income, ok_under.studyRate, ok_under.completionRatio));
+                payimpl.getMonthlyAmount(ok_under.ssn, ok_under.income, ok_under.studyRate, ok_under.completionRatio));
         assertEquals(onlysubsidy,
-                     payimpl.getMonthlyAmount(ok_border.ssn, ok_border.income, ok_border.studyRate, ok_border.completionRatio));
+                payimpl.getMonthlyAmount(ok_border.ssn, ok_border.income, ok_border.studyRate, ok_border.completionRatio));
         assertEquals(0,
-                     payimpl.getMonthlyAmount(notOk_over.ssn, notOk_over.income, notOk_over.studyRate, notOk_over.completionRatio));
+                payimpl.getMonthlyAmount(notOk_over.ssn, notOk_over.income, notOk_over.studyRate, notOk_over.completionRatio));
     }
 
     /**
@@ -82,8 +79,7 @@ public class PaymentTest {
      * From the year 47 = <47
      */
     @Test
-    public void noLoanAfter47() throws IOException
-    {
+    public void noLoanAfter47() throws IOException {
         PaymentImpl payimpl = this.getPaymentImplInstanceCurrentDate(); //FIXME use 2016 + adjust birthdays
 
         Student ok_under = StudentBuilder.fullTimeStudentNoIncome()
@@ -102,16 +98,41 @@ public class PaymentTest {
         int onlySubsidy = TestNumConstants.StudentSupportType.FULL_TIME_SUBSIDIARY.getAmountPerMonth();
 
         assertEquals(fullGrant,
-                     payimpl.getMonthlyAmount(ok_under.ssn, ok_under.income, ok_under.studyRate, ok_under.completionRatio));
+                payimpl.getMonthlyAmount(ok_under.ssn, ok_under.income, ok_under.studyRate, ok_under.completionRatio));
         assertEquals(onlySubsidy,
-                     payimpl.getMonthlyAmount(notOk_border.ssn, notOk_border.income, notOk_border.studyRate, notOk_border.completionRatio));
+                payimpl.getMonthlyAmount(notOk_border.ssn, notOk_border.income, notOk_border.studyRate, notOk_border.completionRatio));
         assertEquals(onlySubsidy,
-                     payimpl.getMonthlyAmount(notOk_over.ssn, notOk_over.income, notOk_over.studyRate, notOk_over.completionRatio));
+                payimpl.getMonthlyAmount(notOk_over.ssn, notOk_over.income, notOk_over.studyRate, notOk_over.completionRatio));
     }
 
     // ---------------------------------------------------------------
     // 200-series requirements
     // ---------------------------------------------------------------
+    @Test
+    public void atLeastHalfTimeStudies() throws IOException {
+        Student fulltimeRate = new StudentBuilder().studyRate(TestNumConstants.StudyRate.FULL_TIME).build();
+        Student lessthanfulltimeRate = new StudentBuilder().studyRate(TestNumConstants.StudyRate.LESS_THAN_FULL_TIME).build();
+        Student halftimeRate = new StudentBuilder().studyRate(TestNumConstants.StudyRate.HALF_TIME).build();
+        Student lessthanhalftimeRate = new StudentBuilder().studyRate(TestNumConstants.StudyRate.LESS_THAN_HALF_TIME).build();
+
+        PaymentImpl payimpl = this.getPaymentImplInstanceCurrentDate();
+
+        int fullGrant = TestNumConstants.StudentSupportType.FULL_TIME_LOAN.getAmountPerMonth() +
+                TestNumConstants.StudentSupportType.FULL_TIME_SUBSIDIARY.getAmountPerMonth();
+        int halfGrant = TestNumConstants.StudentSupportType.PART_TIME_LOAN.getAmountPerMonth() +
+                TestNumConstants.StudentSupportType.PART_TIME_SUBSIDIARY.getAmountPerMonth();
+
+        // Full time
+        assertEquals(fullGrant, payimpl.getMonthlyAmount(fulltimeRate.ssn, fulltimeRate.income, fulltimeRate.studyRate, fulltimeRate.completionRatio));
+
+        // Less than full time, more than half time
+        assertEquals(halfGrant, payimpl.getMonthlyAmount(lessthanfulltimeRate.ssn, lessthanfulltimeRate.income, lessthanfulltimeRate.studyRate, lessthanfulltimeRate.completionRatio));
+        assertEquals(halfGrant, payimpl.getMonthlyAmount(halftimeRate.ssn, halftimeRate.income, halftimeRate.studyRate, halftimeRate.completionRatio));
+
+        // Below half time
+        assertEquals(0, payimpl.getMonthlyAmount(lessthanhalftimeRate.ssn, lessthanhalftimeRate.income, lessthanhalftimeRate.studyRate, lessthanhalftimeRate.completionRatio));
+    }
+
 
     // ---------------------------------------------------------------
     // 300-series requirements
